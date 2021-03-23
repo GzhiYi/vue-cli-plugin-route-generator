a73ba8c81df826d31885b9ca59934ffd2a232fcd
GzhiYi <gongzhiyi@youmi.net>
2021-03-23 11:32:36 +0800

    fix: 修复点配置问题。

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const RouterWebpackPlugin = require('./plugins/Router.webpack.plugin')
-
-const isProduction = process.env.NODE_ENV === 'production'
+const RouterWebpackPlugin = require('./Router.webpack.plugin')
 
 module.exports = (api, projectOptions) => {
   const options = projectOptions.pluginOptions.routeConfig
-  console.log('options', options)
   api.configureWebpack(config => {
     config.plugins.push(new RouterWebpackPlugin(options ? options.route : {} || {}))
   })
